1a4247eb1d05134b816dd5e3598df85cd4c0bea0
Marius4444 <150292448+Marius4444@users.noreply.github.com>
2024-03-18 13:59:28 +0000

    new

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DebugHelperV',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Marius',
+    author_email='',
+    description=''
+)
